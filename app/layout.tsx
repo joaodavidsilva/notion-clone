c90d3f2ae6af2789a1b5a8d79968f3d8cb6bb7c6
João David Silva <ptlghost@gmail.com>
2024-02-08 06:29:17 +0000

    logo works on light and dark mode now (used prefers-color-scheme on root layout metadata)

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,20 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Motion",
   description: "Motion is the connected workspace where better, faster work happens.",
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/logo.svg",
+        href: "/logo.svg"
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/logo-dark.svg",
+        href: "/logo-dark.svg"
+      }
+    ]
+  }
 };
 
 export default function RootLayout({
